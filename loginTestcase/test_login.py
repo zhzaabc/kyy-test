3215ab18f3fa6d75ce9665681506480e5ec49f57
zhzaabc <1248327034@qq.com>
2021-01-14 00:06:38 +0800

    20210113

--- loginTestcase/test_login.py
+++ loginTestcase/test_login.py
@@ -1,1 +1,14 @@
-import pytest,allure
+import pytest,allure,ddt
+from selenium import webdriver
+from utils.excel_read import ParseExcel
+
+excelPath = ''
+sheetName = ''
+excel = ParseExcel(excelPath,sheetName)
+
+@ddt.ddt
+class test_login():
+
+    @ddt.data(*excel.getDataFromSheet())
+    def test_login(self,data):
+        self.driver.find_elements_by_id('').sendkeys('data')
--- utils/excel_read.py
+++ utils/excel_read.py
@@ -1,19 +1,20 @@
 from openpyxl import load_workbook
 
 class ParseExcel():
-    def __init__(self,excel_path):
+    def __init__(self,excel_path,sheetName):
         self.wb = load_workbook(excel_path)
-        self.sheet = self.wb.active
+        self.sheet = self.wb[sheetName]
 
 
     def getDataFromSheet(self):
         datalist = []
         for line in self.sheet:
-            datalist.append(line)
+            datalist.append(line[0].value)
+        datalist.pop(0)
         return datalist
 
-if  __name__ == '__main__':
-    excel_path = './../data/login_excel.xls'
-    sheetName = 'user'
-    parse = ParseExcel(excel_path)
-    print(parse.getDataFromSheet())
+# if  __name__ == '__main__':
+#     excel_path = './../data/login_excel.xls'
+#     sheetName = 'user'
+#     parse = ParseExcel(excel_path,sheetName)
+#     print(parse.getDataFromSheet())
