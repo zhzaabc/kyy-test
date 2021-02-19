1041042db1cd28a00ec4fa5d2a8f4624049fcb26
zhou <1248327034@qq.com>
2021-02-19 11:47:52 +0800

    增加发送邮件功能&环境位置修改

---
+++ config/smtp.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# 配置邮箱SMTP服务器的主机地址&端口
+SMTP_SERVER = 'smtp.qq.com'
+SMTP_PORT='465'
+# # 指定发件人和收件人。
+SMTP_SENDER = '1248327034@qq.com'
+SMTP_RECEIVER = '2818920513@qq.com'
+#邮箱用户名&授权码
+SMTP_USER = '1248327034@qq.com'
+SMTP_PASS='wbhyednsxuvigije'
