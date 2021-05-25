7b6383fc7fc174dc55cd6a68b9c657d97882ac49
zhou <1248327034@qq.com>
2021-05-25 18:13:51 +0800

    update 增加日志功能

--- loginTest/test_login.py
+++ loginTest/test_login.py
@@ -11,29 +11,39 @@ headers = {'content-type': "application/json", 'charset': "UTF-8"}
 class Test_login(object):
 
     @allure.story('打开首页地址')
-    @allure.severity('blocker')
-    @pytest.mark.parametrize('user_name,user_pwd',[(excel_login.getDataFromSheet()[0][0],excel_login.getDataFromSheet()[0][1])])
-    def test_openUrl(self,user_name,user_pwd):
-        # self.driver = webdriver.Chrome()
-        # self.driver.get(test_url)
-        # self.driver.maximize_window()
-        # time.sleep(10)
-        # self.driver.find_element_by_class_name("login").click()
-        # self.driver.find_element_by_xpath("//*[@id='__layout']/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[2]/ul/li[2]/span").click()
-        # self.driver.find_elements_by_tag_name("input")[0].clear()
-        # self.driver.find_elements_by_tag_name("input")[0].send_keys(user_name)
-        # self.driver.find_elements_by_tag_name("input")[1].clear()
-        # self.driver.find_elements_by_tag_name("input")[1].send_keys(user_pwd)
-        # time.sleep(5)
-        # self.driver.find_element_by_class_name("login-submit-btn").click()
-        #####登录请求接口
-        requests_login = requests.post(test_url+'api/v1/login/password',json={'username':str(user_name),"password":str(user_pwd)},headers=headers )
-        # assert requests_login.status_code == 200
-        pytest.main(['-s', '-v', '--alluredir', './../report/html/allure-results'])
+    @allure.severity(allure.severity_level.CRITICAL)
+    def test_openUrl(self):
+        with allure.step('1.打开网站{}'):test_url
+        self.driver = webdriver.Chrome()
+        self.driver.get(test_url)
+        self.driver.maximize_window()
+        time.sleep(5)
+        return self.driver
 
-# if __name__ == '__main__':
-#     pytest.main(['-s','-v','--alluredir','./../report/html/allure-results'])
-    # def test_assert(self,status_code):
-    #     if status_code == 200:
-    #         assert
+    @allure.story('进入登录页面登录')
+    @allure.severity('blocker')
+    @pytest.mark.parametrize('user_name,user_pwd',[(excel_login.getDataFromSheet()[0][0], excel_login.getDataFromSheet()[0][1])])
+    def test_login_user(self,user_name,user_pwd):
+        self.driver.find_element_by_class_name("login").click()
+        self.driver.find_element_by_xpath(
+            "//*[@id='__layout']/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div[2]/ul/li[2]/span").click()
+        self.driver.find_elements_by_tag_name("input")[0].clear()
+        self.driver.find_elements_by_tag_name("input")[0].send_keys(user_name)
+        self.driver.find_elements_by_tag_name("input")[1].clear()
+        self.driver.find_elements_by_tag_name("input")[1].send_keys(user_pwd)
+        time.sleep(5)
+        self.driver.find_element_by_class_name("login-submit-btn").click()
+        ##登录断言
+        time.sleep(0.5)
+        # message = self.driver.find_elements_by_class_name("ivu-message-error")[0].text
+        pytest.main(['-s', '-', '--alluredir', './../report/html/allure-results'])
+
+if __name__ == '__main__':
+    pytest.main(['-s','-v','--alluredir','./../report/html/allure-results'])
+    def test_assert(self,status_code):
+        if self.driver.find_elements_by_class_name("ivu-message-success")[0].text =='登录成功':
+            return "登录成功"
+
+
+
 
